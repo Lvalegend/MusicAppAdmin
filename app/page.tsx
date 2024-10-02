657a74bfc969ed53ec4a2db9af580c64abee3c23
DucThanh <phoducthanh@gmail.com>
2024-10-02 22:17:59 +0700

    First commit in branch dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "antd";
-import Image from "next/image";
+import { Button } from 'antd';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <Button>
-        Lê Văn An
-      </Button>
+      <Button>Lê Văn An</Button>
     </div>
   );
 }
